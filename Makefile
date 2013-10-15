a53a284096b92060bbb8f3cb002faa016ca251bc
evan-gu <guyifan1121@gmail.com>
2013-10-15 00:07:46 -0400

    packet module and test

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 IDIR =src/include
 ODIR=src/obj
 SRCDIR=src
+TESTDIR=src/test
 
 CC 		= gcc
 CFLAGS=-I$(IDIR) -Wall -Werror -DDEBUG
@@ -9,11 +10,11 @@ LDFLAGS	= -lm
 TESTDEFS	= -DTESTING			# comment this out to disable debugging code
 
 _HEADERS = bt_parse.h  chunk.h  debug.h  debug-text.h  input_buffer.h  sha.h  spiffy.h \
-		   logger.h
+		   logger.h  packet.h
 HEADERS = $(patsubst %,$(IDIR)/%,$(_HEADERS))
 
 _OBJS = peer.o bt_parse.o spiffy.o debug.o input_buffer.o chunk.o sha.o \
-		logger.o
+		logger.o 
 OBJS = $(patsubst %,$(ODIR)/%,$(_OBJS))
 
 _MK_CHUNK_OBJS   = make_chunks.o chunk.o sha.o
@@ -23,7 +24,10 @@ TESTDEFS	= -DTESTING			# comment this out to disable debugging code
 
 BINS = peer make-chunks
 
+TESTBINS = test_packet
+
 # Explit build and testing targets
+test: ${TESTBINS}
 
 all: ${BINS} ${TESTBINS}
 
@@ -41,3 +45,5 @@ clean:
 	@rm -f $(BINS) \
 		$(ODIR)/* $(SRCDIR)/*~ $(IDIR)/*~ $(SRCDIR)/*.orig $(IDIR)/*.orig
 
+test_packet:
+	$(CC) -I$(IDIR) $(TESTDIR)/test_packet.c $(SRCDIR)/chunk.c $(SRCDIR)/sha.c -o test_packet
---
+++ src/include/packet.h
@@ -0,0 +1,76 @@
+#ifndef _PACKET_H_
+#define _PACKET_H_
+
+#include <stdint.h>
+#include <string.h>
+
+#include "sha.h"
+
+/**
+ * The hashcode in binary
+ */
+char hash_bin_buf[SHA1_HASH_SIZE];
+
+#define MAGIC 15441
+#define VERSION 1
+#define HEADER_SIZE 16
+#define PACKET_SIZE 1500 // udp packet size
+#define PAYLOAD_SIZE (PACKET_SIZE - HEADER_SIZE)
+
+/**
+ * Decode / Encode packet from / to buffer
+ * @param buf, the raw buf recvfrom peers, it's uint8_t*
+ * @param pkt, a pointer to a packet_t struct
+ */
+#define DECODE_PKT(buf, pkt, size) (memcpy((pkt), (buf), (size)))
+#define ENCODE_PKT(buf, pkt, size) (memcpy((buf), (pkt), (size)))
+
+/**
+ * Get / Set chunk numbers
+ * @param pkt, a pointer to a packet_t struct
+ * @param cnt, the chunk number to be set
+ */
+#define GET_CHUNK_CNT(pkt) ((pkt)->payload[0])
+#define SET_CHUNK_CNT(pkt, cnt) ((pkt)->payload[0] = (cnt))
+
+/**
+ * GET / Set hash string
+ * "4" is the size of chunk_numbers
+ * @param pkt, a pointer to a packet_t struct
+ * @param hexbuf, the buffer for saving the result HASH string
+ * @param hash, the HASH string
+ */
+#define GET_HASH(pkt, n, hexbuf) binary2hex(((pkt)->payload+4+(n)*SHA1_HASH_SIZE), SHA1_HASH_SIZE, (hexbuf))
+
+#define SET_HASH(pkt, n, hash)                                          \
+    hex2binary((hash), SHA1_HASH_SIZE*2, hash_bin_buf);                 \
+    memcpy((pkt)->payload+4+(n)*SHA1_HASH_SIZE, hash_bin_buf, SHA1_HASH_SIZE)
+
+/**
+ * types of a packet
+ */
+enum packet_type {
+    PACKET_TYPE_WHOHAS = 0,
+    PACKET_TYPE_IHAVE = 1,
+    PACKET_TYPE_DATA = 2,
+    PACKET_TYPE_GET = 3,
+    PACKET_TYPE_ACK = 4,
+    PACKET_TYPE_DENIED = 5,
+};
+
+/**
+ * the packet struct
+ */
+typedef struct packet_s {
+    uint16_t magic;
+    uint8_t version;
+    uint8_t type;
+    uint16_t hdr_len;
+    uint16_t pkt_len;
+    uint32_t seq;
+    uint32_t ack;
+    
+    uint8_t payload[PAYLOAD_SIZE]; // either DATA or HASHs
+} packet_t;
+
+#endif
